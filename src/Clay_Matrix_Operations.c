48818e24085b228c78294d257cb5fdf063bfbb38
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-10-03 22:17:40 +0200

    Update Clay_Matrix_Operations.c

--- src/Clay_Matrix_Operations.c
+++ src/Clay_Matrix_Operations.c
@@ -101,20 +101,20 @@ void subMatrixMatrix(Matrix *matrix0, const Matrix *matrix1) {
  * @brief Row swap.
  * 
  * @param matrix Matrix.
- * @param r0 Row index.
- * @param r1 Row index.
+ * @param n0 Row index.
+ * @param n1 Row index.
  */
-void swapRows(Matrix *matrix, const Natural r0, const Natural r1) {
+void swapRows(Matrix *matrix, const Natural n0, const Natural n1) {
     #ifndef NDEBUG // Integrity check.
-    assert(r0 < matrix->N);
-    assert(r1 < matrix->N);
+    assert(n0 < matrix->N);
+    assert(n1 < matrix->N);
     #endif
 
-    if(r0 != r1)
+    if(n0 != n1)
         for(Natural k = 0; k < matrix->M; ++k) {
-            Real temp = matrix->elements[r0 * matrix->M + k];
-            matrix->elements[r0 * matrix->M + k] = matrix->elements[r1 * matrix->M + k];
-            matrix->elements[r1 * matrix->M + k] = temp;
+            Real temp = matrix->elements[n0 * matrix->M + k];
+            matrix->elements[n0 * matrix->M + k] = matrix->elements[n1 * matrix->M + k];
+            matrix->elements[n1 * matrix->M + k] = temp;
         }
 }
 
@@ -122,20 +122,20 @@ void swapRows(Matrix *matrix, const Natural r0, const Natural r1) {
  * @brief Column swap.
  * 
  * @param matrix Matrix.
- * @param c0 Column index.
- * @param c1 Column index.
+ * @param m0 Column index.
+ * @param m1 Column index.
  */
-void swapColumns(Matrix *matrix, const Natural c0, const Natural c1) {
+void swapColumns(Matrix *matrix, const Natural m0, const Natural m1) {
     #ifndef NDEBUG // Integrity check.
-    assert(c0 < matrix->M);
-    assert(c1 < matrix->M);
+    assert(m0 < matrix->M);
+    assert(m1 < matrix->M);
     #endif
 
-    if(c0 != c1)
+    if(m0 != m1)
         for(Natural j = 0; j < matrix->N; ++j) {
-            Real temp = matrix->elements[j * matrix->M + c0];
-            matrix->elements[j * matrix->M + c0] = matrix->elements[j * matrix->M + c1];
-            matrix->elements[j * matrix->M + c1] = temp;
+            Real temp = matrix->elements[j * matrix->M + m0];
+            matrix->elements[j * matrix->M + m0] = matrix->elements[j * matrix->M + m1];
+            matrix->elements[j * matrix->M + m1] = temp;
         }
 }
 
