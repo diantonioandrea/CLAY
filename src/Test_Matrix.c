ca4268de6600467cc6be4a43dbccdbe15004fa49
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-10-03 20:45:02 +0200

    Update Test_Matrix.c

--- src/Test_Matrix.c
+++ src/Test_Matrix.c
@@ -13,15 +13,42 @@
 int main(int argc, char **argv) {
 
     Matrix *m0 = newMatrix(2, 2);
+    Matrix *m1 = newMatrix(2, 2);
+
+    Vector *v0 = newVector(2);
 
     setMatrixAt(m0, 0, 0, 1.0L);
     setMatrixAt(m0, 0, 1, 2.0L);
     setMatrixAt(m0, 1, 0, 3.0L);
     setMatrixAt(m0, 1, 1, 4.0L);
 
+    setMatrixAt(m1, 0, 0, 5.0L);
+    setMatrixAt(m1, 0, 1, 6.0L);
+    setMatrixAt(m1, 1, 0, 7.0L);
+    setMatrixAt(m1, 1, 1, 8.0L);
+
+    setVectorAt(v0, 0, 1.0L);
+    setVectorAt(v0, 1, 2.0L);
+
+    Matrix *m2 = mulReturnMatrixMatrix(m0, m1);
+
+    Vector *v1 = mulReturnMatrixVector(m1, v0);
+    Vector *v2 = mulReturnVectorMatrix(v0, m1);
+
     printMatrix(m0);
+    printMatrix(m1);
+    printMatrix(m2);
+
+    printVector(v1);
+    printVector(v2);
 
     freeMatrix(m0);
+    freeMatrix(m1);
+    freeMatrix(m2);
+
+    freeVector(v0);
+    freeVector(v1);
+    freeVector(v2);
 
     return 0;
 }
