8b530c7b08064d36ec83c5b358611cad2dfcc067
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-10-09 11:45:42 +0200

    Started working on sparse matrices

---
+++ include/Sparse/Sparse.h
@@ -0,0 +1,142 @@
+/**
+ * @file Sparse.h
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief Sparse matrices
+ * @date 2024-10-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef CLAY_SPARSE_SPARSE
+#define CLAY_SPARSE_SPARSE
+
+#include "../Matrix.h"
+
+typedef struct {
+    
+    /**
+     * @brief Sparse's rows.
+     * 
+     */
+    Natural N;
+
+    /**
+     * @brief Sparse's columns.
+     * 
+     */
+    Natural M;
+
+    /**
+     * @brief Sparse's size.
+     * 
+     */
+    Natural S;
+
+    /**
+     * @brief Sparse's indices.
+     * 
+     */
+    Natural *indices;
+
+    /**
+     * @brief Sparse's elements.
+     * 
+     */
+    Real *elements;
+
+} Sparse;
+
+typedef struct {
+
+    /**
+     * @brief Sparse's rows.
+     * 
+     */
+    Natural N;
+
+    /**
+     * @brief Sparse's columns.
+     * 
+     */
+    Natural M;
+
+    /**
+     * @brief Sparse's inner indices.
+     * 
+     */
+    Natural *inner;
+
+    /**
+     * @brief Sparse's outer indices.
+     * 
+     */
+    Natural *outer;
+
+    /**
+     * @brief Sparse's elements.
+     * 
+     */
+    Natural *elements;
+
+} SparseCSR;
+
+typedef struct {
+
+    /**
+     * @brief Sparse's rows.
+     * 
+     */
+    Natural N;
+
+    /**
+     * @brief Sparse's columns.
+     * 
+     */
+    Natural M;
+
+    /**
+     * @brief Sparse's inner indices.
+     * 
+     */
+    Natural *inner;
+
+    /**
+     * @brief Sparse's outer indices.
+     * 
+     */
+    Natural *outer;
+
+    /**
+     * @brief Sparse's elements.
+     * 
+     */
+    Natural *elements;
+
+} SparseCSC;
+
+// Construction.
+
+[[nodiscard]] Sparse *newSparse(const Natural, const Natural);
+[[nodiscard]] Sparse *newSparseCSR(const Sparse *);
+[[nodiscard]] Sparse *newSparseCSC(const Sparse *);
+
+void freeSparse(Sparse *);
+void freeSparseCSR(SparseCSR *);
+void freeSparseCSC(SparseCSC *);
+
+// Access.
+
+Real getSparseAt(const Sparse *, const Natural, const Natural);
+void setSparseAt(Sparse *, const Natural, const Natural, const Real);
+
+Real getSparseCSRAt(const SparseCSR *, const Natural, const Natural);
+Real getSparseCSCAt(const SparseCSC *, const Natural, const Natural);
+
+// Output.
+
+void printSparse(const Sparse *);
+void printSparseCSR(const SparseCSR *);
+void printSparseCSC(const SparseCSC *);
+
+#endif
